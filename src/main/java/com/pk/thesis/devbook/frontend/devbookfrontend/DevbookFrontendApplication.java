72809d9951e3a8f384329a25152ea7b71ce5f751
gmexo <zidaah@gmail.com>
2020-01-09 21:20:10 +0100

    Initial commit

---
+++ src/main/java/com/pk/thesis/devbook/frontend/devbookfrontend/DevbookFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.pk.thesis.devbook.frontend.devbookfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevbookFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevbookFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pk/thesis/devbook/frontend/devbookfrontend/DevbookFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.thesis.devbook.frontend.devbookfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevbookFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
